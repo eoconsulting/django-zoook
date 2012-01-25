d0de4b0198d4bb01ebc9ee61359fa04167d8117f
Vincent <thomas@domatix.com>
2012-01-25 17:02:24 +0100

    basic setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-zoook',
+    description = '',
+    download_url = 'https://www.github.com/fuzzy-id/django-zoook',
+    install_requires = [
+        'django-localeurl',
+        'django-transmeta',
+        'paramiko',
+        'recaptcha-client',
+        'Pyro'
+        ]
+    requires = [
+        'ooop',
+        'django-inplaceedit',
+        'django-maintenancemode'
+        ],
+    dependency_links = [
+        'https://github.com/zikzakmedia/ooop.git',
+        'https://github.com/zikzakmedia/django-inplaceeditform.git',
+        'https://github.com/zikzakmedia/django-maintenancemode.git'
+        ]
+
+)
