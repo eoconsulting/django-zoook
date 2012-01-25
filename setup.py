8f03d7be6581010349c6f099e4d88908be306756
Vincent <thomas@domatix.com>
2012-01-25 17:29:08 +0100

    avoiding '-'

--- setup.py
+++ setup.py
@@ -3,25 +3,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-zoook',
+    name = 'django_zoook',
     description = '',
     download_url = 'https://www.github.com/fuzzy-id/django-zoook',
     install_requires = [
-        'django-localeurl',
-        'django-transmeta',
+        'django_localeurl',
+        'django_transmeta',
         'paramiko',
-        'recaptcha-client',
+        'recaptcha_client',
         'Pyro'
         ],
     requires = [
         'ooop',
-        'django-inplaceedit',
-        'django-maintenancemode'
-        ],
-    dependency_links = [
-        'https://github.com/zikzakmedia/ooop.git',
-        'https://github.com/zikzakmedia/django-inplaceeditform.git',
-        'https://github.com/zikzakmedia/django-maintenancemode.git'
+        'django_inplaceedit',
+        'django_maintenancemode'
         ]
-
 )
