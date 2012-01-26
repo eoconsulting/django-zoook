916d450e1ae0d0a3a2512f5aac5a2cdefad9a762
Vincent <thomas@domatix.com>
2012-01-26 13:21:36 +0100

    corrected package name

--- django_zoook/tools/cms/templatetags/modules.py
+++ django_zoook/tools/cms/templatetags/modules.py
@@ -22,7 +22,7 @@
 
 from django import template
 from django.template import Library, Node
-from zoook.tools.cms.models import Modules
+from django_zoook.tools.cms.models import Modules
 
 register = template.Library()
 
