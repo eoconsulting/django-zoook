7b65cb84b375fa5918774886c8db0274de94a584
Vincent <thomas@domatix.com>
2012-01-26 11:59:45 +0100

    typo

--- django_zoook/settings.py
+++ django_zoook/settings.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ############################################################################################
 #
 #    Zoook e-sale for OpenERP, Open Source Management Solution	
