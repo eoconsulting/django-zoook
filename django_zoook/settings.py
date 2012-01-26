e62372482b09d2923703544e3135b4afcbeed9e7
Vincent <thomas@domatix.com>
2012-01-26 12:11:01 +0100

    example.com should be used as example site

--- django_zoook/settings.py
+++ django_zoook/settings.py
@@ -69,7 +69,7 @@ MEDIA_ROOT = os.path.join(PATH, "static")
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
+# Examples: "http://example.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
