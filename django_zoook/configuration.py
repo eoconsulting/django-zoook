c3785474ca141988b6be536517968e062988808b
Vincent <thomas@domatix.com>
2012-02-02 10:37:13 +0100

    workaround for "cannot find 'settings.py'"

--- django_zoook/configuration.py
+++ django_zoook/configuration.py
@@ -25,7 +25,7 @@ import os
 import sys
 
 sys.path.append('.')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_zoook.settings'
 
 from settings import *
 from django.utils.translation import ugettext as _
